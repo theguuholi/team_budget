130a0fe77bdaff7561c7022e749657ad06884db5
Gustavo <g.92oliveira@gmail.com>
2021-06-03 16:36:17 -0300

    format code

--- lib/team_budget/accounts/core/create_user.ex
+++ lib/team_budget/accounts/core/create_user.ex
@@ -28,17 +28,27 @@ defmodule TeamBudget.Accounts.Core.CreateUser do
   end
 
   defp include_user_as_a_member_in_a_team(multi, team_and_invites) do
-    multi = Ecto.Multi.insert_all(multi, :include_user_as_member, Member, fn %{user: user} ->
-      Enum.map(team_and_invites, &%{user_id: user.id, team_id: &1.team.id, inserted_at: build_date(), updated_at: build_date()})
-    end)
+    multi =
+      Ecto.Multi.insert_all(multi, :include_user_as_member, Member, fn %{user: user} ->
+        Enum.map(
+          team_and_invites,
+          &%{
+            user_id: user.id,
+            team_id: &1.team.id,
+            inserted_at: build_date(),
+            updated_at: build_date()
+          }
+        )
+      end)
+
     {multi, team_and_invites}
   end
 
   defp build_date(), do: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 
   defp delete_invites({multi, team_and_invites}) do
-    Ecto.Multi.delete_all(multi, :delete_invites, fn _  ->
-      invites = Enum.map(team_and_invites, &(&1.invite_id))
+    Ecto.Multi.delete_all(multi, :delete_invites, fn _ ->
+      invites = Enum.map(team_and_invites, & &1.invite_id)
       from i in Invite, where: i.id in ^invites
     end)
   end
